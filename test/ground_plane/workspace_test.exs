24b5d464117ded8fc3684eb89063300226846fa5
nshkrdotcom <127063941+nshkrdotcom@users.noreply.github.com>
2026-05-10 06:29:37 -1000

    Update Weld to 0.8.2

--- test/ground_plane/workspace_test.exs
+++ test/ground_plane/workspace_test.exs
@@ -17,7 +17,7 @@ defmodule GroundPlane.WorkspaceTest do
   end
 
   test "uses the released Weld line directly" do
-    assert {:weld, "~> 0.8.1", only: [:dev, :test], runtime: false} in MixProject.project()[:deps]
+    assert {:weld, "~> 0.8.2", only: [:dev, :test], runtime: false} in MixProject.project()[:deps]
   end
 
   test "does not select workspace concurrency from environment variables" do
